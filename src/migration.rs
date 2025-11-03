105082346b5d95c9bc54e53a5326587a163ccaea
JQiue <861947542@qq.com>
2025-11-03 20:56:38 +0800

    feat: support postgresql backend

--- src/migration.rs
+++ src/migration.rs
@@ -1,8 +1,14 @@
 use migration::{Migrator, MigratorTrait};
-use sea_orm::{DatabaseConnection, DbErr};
+use sea_orm::{ConnectionTrait, DatabaseConnection, DbErr};
+use tracing::info;
 
 pub async fn migrate(database_url: &str) -> Result<DatabaseConnection, DbErr> {
   let connection = sea_orm::Database::connect(database_url).await?;
+  match connection.get_database_backend() {
+    sea_orm::DatabaseBackend::Sqlite => info!("Using SQLite"),
+    sea_orm::DatabaseBackend::MySql => info!("Using MySQL/MariaDB"),
+    sea_orm::DatabaseBackend::Postgres => info!("Using PostgreSQL"),
+  }
   Migrator::up(&connection, None).await?;
   Ok(connection)
 }
